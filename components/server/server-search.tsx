90aab82fe18687ea6f6256cd34076e2f138bc53a
ShayC0derBui <sinhaprem636@gmail.com>
2024-03-26 04:19:48 +0530

    params null error

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -17,12 +17,12 @@ interface ServerSeaarchProps {
     label: string;
     type: "channel" | "member";
     data:
-      | {
-          icon: React.ReactNode;
-          name: string;
-          id: string;
-        }[]
-      | undefined;
+    | {
+      icon: React.ReactNode;
+      name: string;
+      id: string;
+    }[]
+    | undefined;
   }[];
 }
 
@@ -52,10 +52,10 @@ export const ServerSearch = ({ data }: ServerSeaarchProps) => {
     setOpen(false);
 
     if (type === "member") {
-      router.push(`/servers/${params.serverId}/conversations/${id}`);
+      router.push(`/servers/${params?.serverId}/conversations/${id}`);
     }
     if (type === "channel") {
-      router.push(`/servers/${params.serverId}/channels/${id}`);
+      router.push(`/servers/${params?.serverId}/channels/${id}`);
     }
   };
 
@@ -63,13 +63,21 @@ export const ServerSearch = ({ data }: ServerSeaarchProps) => {
     <>
       <button
         onClick={() => setOpen(true)}
-        className="group flex w-full items-center gap-x-2 rounded-md px-2 py-2 transition hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50"
+        className="group flex w-full items-center gap-x-2 rounded-md px-2 py-2 transition
+          hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50"
       >
         <Search className="h-4 w-4 text-zinc-500 dark:text-zinc-400" />
-        <p className="text-sm font-semibold text-zinc-500 transition group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300">
+        <p
+          className="text-sm font-semibold text-zinc-500 transition group-hover:text-zinc-600
+            dark:text-zinc-400 dark:group-hover:text-zinc-300"
+        >
           Search
         </p>
-        <kbd className="pointer-events-none ml-auto inline-flex h-5 select-none gap-1 rounded border bg-muted px-1.5  text-center font-mono text-[10px] font-medium text-muted-foreground">
+        <kbd
+          className="pointer-events-none ml-auto inline-flex h-5 select-none gap-1 rounded border
+            bg-muted px-1.5 text-center font-mono text-[10px] font-medium
+            text-muted-foreground"
+        >
           <span className="text-xs">⌘</span>K
         </kbd>
       </button>
@@ -84,7 +92,10 @@ export const ServerSearch = ({ data }: ServerSeaarchProps) => {
               <CommandGroup key={label} heading={label}>
                 {data?.map(({ id, icon, name }) => {
                   return (
-                    <CommandItem key={id} onSelect={() => onClick({id, type})}>
+                    <CommandItem
+                      key={id}
+                      onSelect={() => onClick({ id, type })}
+                    >
                       {icon}
                       <span>{name}</span>
                     </CommandItem>
